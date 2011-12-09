1ebfe2e07008e586f7e9c05004c21c146ea45a78
Jeremy Kahn <jeremyckahn@jeremyckahn-ubuntu.(none)>
2011-12-08 22:39:34 -0800

    Removed Vim stuff.

--- vim/install_pathogen.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.vim/autoload;
-curl -so ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
-
-cd ~/tools/;
-git submodule init;
--- vim/update_vim_plugins.sh
+++
@@ -1,2 +0,0 @@
-cd ~/tools
-git submodule update
